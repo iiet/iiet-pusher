e21fde6faa3ed99e43334981c20063847496aec1
kruczjak <kruczjak@gmail.com>
2016-04-25 14:34:17 +0200

    Small fixes

--- lib/iiet_forum_pusher.rb
+++ lib/iiet_forum_pusher.rb
@@ -9,17 +9,17 @@ module IietPusher
 
     def process
       @settings['forum_feeds'].each do |subject|
-        last_sync = ForumTime.last_sync_time(subject[0]).time
+        last_sync = ForumTime.last_sync_time(subject[0])&.time
 
         atom_feed = @forum_interactor.get(subject[1])
         rss = Rss.new(atom_feed)
         newest_time = rss.newest_time
 
         ForumTime.create(atom_id: subject[0], time: newest_time) if last_sync.nil? || last_sync <= newest_time
-        return if last_sync.nil?
+        next if last_sync.nil?
 
         items = rss.only_newest(last_sync).reverse
-        return if items.empty?
+        next if items.empty?
 
         NotifierWorker.perform_async(
           subject[2],
